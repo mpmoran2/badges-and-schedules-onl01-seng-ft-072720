2d18e5ba7f9f810d12e75cd965d7a2bba3eaf948
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-29 23:31:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -32,3 +33,25 @@ end
 
 
 
+=======
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+badge_maker("Arel")
+
+def batch_badge_creator
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do |name|
+    puts "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  counter = 1
+  names.each do |name|
+    puts "Hello, #{name}. You'll be assigned to room #{counter}"
+end
+end
+>>>>>>> 280d96fe330e24722734839c7b57cab46e106c72
